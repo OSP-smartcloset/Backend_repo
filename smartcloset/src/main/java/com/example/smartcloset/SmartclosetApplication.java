103ed1f2b7ff6b845e4707f1a9f144263dd31a1a
hyeonjune <annhj980@naver.com>
2024-08-20 00:23:40 +0900

    Update SmartclosetApplication.java

--- smartcloset/src/main/java/com/example/smartcloset/SmartclosetApplication.java
+++ smartcloset/src/main/java/com/example/smartcloset/SmartclosetApplication.java
@@ -1,4 +1,4 @@
-package com.example.smartcloset.chat;
+package com.example.smartcloset;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
